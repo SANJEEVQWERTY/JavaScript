d400217acb90d7e9b5e0117077b9bb8d76a143e7
SANJEEV <159331593+SANJEEVQWERTY@users.noreply.github.com>
2025-03-02 15:41:56 +0000

    again submit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,28 @@
+const accountId = 12345;
+console.log(accountId)
+
+let accoutEmail = "ddfdfefgj23@ gmail.com";
+console.log(accoutEmail);
+
+var accountPassword = "wweidkds";
+console.log(accountPassword);
+
+accountCity = "uttar Pradesh";
+console.log(accountCity);
+
+let accountState;
+
+//accountId = 2;          // not allowed
+// console.log(accountId)    // Give error message because accountId given const which any variable is declared const they cann't change value
+ 
+accoutEmail = "markJukarbarg345@gmail.com";
+accountPassword = "mmmmmmmmmmmmm";
+accountCity = "dubai"
+
+console.table([accountId, accoutEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+ do not use 'var' because of issue in block scope and functinal scope
+
+*/
